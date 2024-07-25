a4a4a226bb48def053c19929e8841135d8921ced
AnshuKumar45 <anshu852499@gmail.com>
2024-07-25 19:32:13 +0530

    folder added

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'flutter demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
---
+++ lib/views/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScren extends StatelessWidget {
+  const HomeScren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
